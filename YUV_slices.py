ebcc119fa4723a65e20274bd78bb9d65fd17fca5
guidoAI <guido.de.croon@gmail.com>
2019-02-21 18:00:29 +0100

    Changed the exercise based on the lecture experience and questions

--- YUV_slices.py
+++ YUV_slices.py
@@ -38,7 +38,7 @@ def YUV_to_RGB(im):
         rgb[inds2[0][i], inds2[1][i], inds2[2][i]] = 1.0;
     return rgb;
 
-def generate_slices_YUV(n_slices = 5, H = 400, W = 400):
+def generate_slices_YUV(n_slices = 5, H = 255, W = 255):
     """ Generate YUV slices and convert them to RGB to show them """
 #    bgr = np.zeros([H, W, 3]);
 #    bgr[:,:,2] = 1.0;
@@ -67,6 +67,8 @@ def generate_slices_YUV(n_slices = 5, H = 400, W = 400):
         rgb = YUV_to_RGB(im);
         plt.figure()
         plt.imshow(rgb);
+        plt.xlabel('U');
+        plt.ylabel('V');
         plt.title('Y = ' + str(Y));
 
 #        im=im.astype(np.float32)
@@ -87,8 +89,15 @@ def filter_color(image_name = 'DelFly_tulip.jpg', y_low = 50, y_high = 200, \
                YUV[y,x,1] >= v_low and YUV[y,x,1] <= v_high):
                 Filtered[y,x] = 1;
     
+    plt.figure();
+    RGB = cv2.cvtColor(im, cv2.COLOR_BGR2RGB);
+    plt.imshow(RGB);
+    plt.title('Original image');
+    
     plt.figure()
     plt.imshow(Filtered);
+    plt.title('Filtered image');
+    
 
 if __name__ == '__main__':
     generate_slices_YUV();
