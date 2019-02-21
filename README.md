2f2bfa1bfe4b6cdc5893b6a7b74cd07e5a16df35
guidoAI <guido.de.croon@gmail.com>
2019-02-21 09:19:37 +0100

    Added color filtering

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # YUV_notebook
 Tiny notebook to get some insight into the YUV color space
+
+When using DelFly_tulip.jpg, please credit: "TU Delft, photo by Matej Karasek".
--- YUV_slices.py
+++ YUV_slices.py
@@ -7,7 +7,7 @@ Generate and show slices of YUV
 @author: LocalAdmin
 """
 
-#import cv2;
+import cv2;
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -68,9 +68,28 @@ def generate_slices_YUV(n_slices = 5, H = 400, W = 400):
         plt.figure()
         plt.imshow(rgb);
         plt.title('Y = ' + str(Y));
-        
+
 #        im=im.astype(np.float32)
 #        bgr = cv2.cvtColor(im, cv2.COLOR_YUV2BGR);
 #        cv2.imshow('BGR', bgr); cv2.waitKey();
+    
+
+def filter_color(image_name = 'DelFly_tulip.jpg', y_low = 50, y_high = 200, \
+                 u_low = 120, u_high = 130, v_low = 120, v_high = 130, resize_factor=1):
+    im = cv2.imread(image_name);
+    im = cv2.resize(im, (int(im.shape[1]/resize_factor), int(im.shape[0]/resize_factor)));
+    YUV = cv2.cvtColor(im, cv2.COLOR_BGR2YUV);
+    Filtered = np.zeros([YUV.shape[0], YUV.shape[1]]);
+    for y in range(YUV.shape[0]):
+        for x in range(YUV.shape[1]):
+            if(YUV[y,x,0] >= y_low and YUV[y,x,0] <= y_high and \
+               YUV[y,x,1] >= u_low and YUV[y,x,1] <= u_high and \
+               YUV[y,x,1] >= v_low and YUV[y,x,1] <= v_high):
+                Filtered[y,x] = 1;
+    
+    plt.figure()
+    plt.imshow(Filtered);
+
 if __name__ == '__main__':
     generate_slices_YUV();
+    filter_color();
